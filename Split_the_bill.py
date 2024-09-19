eaf22367f2a7585ace155db9ccd396b5fc667e22
Dharmendra32 <dharm.singh32@gmail.com>
2024-09-19 19:20:13 +0530

    Update Split_the_bill.py

--- Split_the_bill.py
+++ Split_the_bill.py
@@ -1,3 +1,7 @@
-bmi = int(input("what is your weight?\n"))/(float(input("What is your height?\n")))**2
-True_BMI = round(bmi,2)
-print("Your BMI is: " + str(True_BMI))
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill?$"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill?"))
+
+bill_with_tip = bill * (1 + tip / 100)
+print(round(bill_with_tip/people, 2))
