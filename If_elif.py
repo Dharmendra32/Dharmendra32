c0f5c4ae9cd173b3e2a262fb43f72dbcb4b551f0
Dharmendra Singh <dharm.singh32@gmail.com>
2024-10-06 18:24:18 +0530

    Update If_elif.py

--- If_elif.py
+++ If_elif.py
@@ -1,9 +1,9 @@
 for n in range(1, 101):
-    if n%2 == 0:
-        print("buzz")
+    if n%3 and n%5 == 0:
+        print("Fizzbuzz")
     elif n%3 == 0:
-        print("fizzbuzz")
+        print("Fizz")
     elif n%5 == 0:
-        print(("buzzfizz"))
+        print(("Buzz"))
     else:
         print(n)
